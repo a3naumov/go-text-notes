aa11ad11597532ba1043a423249654f890cdb78f
Artem Naumov <naumovartem272@gmail.com>
2025-01-12 18:24:19 +0400

    Add new routes

--- main.go
+++ main.go
@@ -9,9 +9,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Notes"))
 }
 
+func noteView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific note"))
+}
+
+func noteCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new note"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/note/view", noteView)
+	mux.HandleFunc("/note/create", noteCreate)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
